ed1e9185a270a45f4be81ae451bb53fdf93e0ee4
Mike Moore <tigerhollowpoint@gmail.com>
2016-03-24 14:06:46 -0400

    form addition

--- template-contact.php
+++ template-contact.php
@@ -12,4 +12,18 @@ $contacts = get_post_meta( $post->ID, 'contact-list', true );
 contactConstructor($contacts);
 ?>
 
+
+<div id="contact-form">
+<?php
+$all_forms = ninja_forms_get_all_forms();
+
+if ( function_exists( 'ninja_forms_display_form' ) ) {
+  ninja_forms_display_form( $all_forms[0]['id'] );
+}
+
+
+?>
+
+</div>
+
 <?php include 'footer.php'; ?>
